bb1e5d1dbcf86da6de8bd6ca557494e43982b0e3
Andrew Larson <Andrewlarson1989@gmail.com>
2023-03-18 11:05:06 -0500

    make slot machine function

--- slot.js
+++ slot.js
@@ -1,5 +1,11 @@
+const spinButton = document.getElementById("spin");
+const reelOne = document.getElementById("reelOne");
+const reelTwo = document.getElementById("reelTwo");
+const reelThree = document.getElementById("reelThree");
+const outcome = document.getElementById("outcome");
+
 const spinReel = () => {
-   let randomNumber = Math.floor(Math.random()*10);
+   let randomNumber = Math.floor(Math.random()*7);
    switch (randomNumber) {
     case 0:
         return "andy";
@@ -11,7 +17,7 @@ const spinReel = () => {
         return "ryder";
         break;
     case 3:
-        return "oaken";
+        return "new baby";
         break;
     case 4:
         return "nala";
@@ -22,7 +28,7 @@ const spinReel = () => {
     case 6:
         return "pizza";
         break;
-    case 7:
+    /*case 7:
         return "games";
         break;
     case 8:
@@ -30,6 +36,56 @@ const spinReel = () => {
         break;
     case 9:
         return "fairy";
-        break;
+        break;*/
    }
 }
+
+spinButton.onclick = function() {
+    let win = 0;
+    const reelOneOutcome = spinReel();
+    const reelTwoOutcome = spinReel();
+    const reelThreeOutcome = spinReel();
+    reelOne.innerHTML = reelOneOutcome;
+    reelTwo.innerHTML = reelTwoOutcome;
+    reelThree.innerHTML = reelThreeOutcome;
+    if (reelOneOutcome === reelTwoOutcome && reelTwoOutcome === reelThreeOutcome) {
+        switch (reelOneOutcome) {
+            case "andy":
+                win += 500;
+                break;
+            case "kelly":
+                win += 400;
+                break;
+            case "ryder":
+                win += 300;
+                break;
+            case "new baby":
+                win += 200;
+                break;
+            case "nala":
+                win += 100;
+                break;
+            case "mufasa":
+                win += 100;
+                break;
+            case "pizza":
+                win += 75;
+                break;
+        }
+        document.getElementById("outcome").innerHTML = `YOU WIN ${win}!!!`
+    } else if (reelOneOutcome === "pizza" || reelTwoOutcome === "pizza" || reelThreeOutcome === "pizza") {
+        if (reelOneOutcome === "pizza") {
+            win += 10
+        };
+        if (reelTwoOutcome === "pizza") {
+            win += 10
+        };
+        if (reelThreeOutcome === "pizza") {
+            win += 10
+        };
+        document.getElementById("outcome").innerHTML = `YOU WIN ${win}!!!`
+    } else {
+        document.getElementById("outcome").innerHTML = "try again"
+    }
+}
+
